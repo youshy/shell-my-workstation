8aa60c624655b2526e9929ae28b5194360cd0b47
Artur Kondas <devilyoushy@gmail.com>
2019-07-19 14:14:30 +0100

    Update set-wallpaper.sh

--- scripts/common/set-wallpaper.sh
+++ scripts/common/set-wallpaper.sh
@@ -1,4 +1,8 @@
 # Part of AppleScript to set up wallpaper
 # Needs to go to the main file though
 
+# This solution doesn't work
 osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/path/to/pic.jpg"'
+
+# Try this
+# https://www.tech-otaku.com/mac/setting-desktop-image-macos-mojave-from-command-line/
