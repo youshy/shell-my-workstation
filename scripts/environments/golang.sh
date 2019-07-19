fdec300138dbb70bae008915b8023ce54084d7d2
Artur Kondas <devilyoushy@gmail.com>
2019-07-19 10:49:00 +0100

    Trial run
    getting go packages doesn't work

--- scripts/environments/golang.sh
+++ scripts/environments/golang.sh
@@ -4,6 +4,7 @@ echo "Installing Golang Development tools"
 mkdir -p ~/go/src
 brew install go
 
-go get golang.org/x/tools/cmd/godoc
-go get github.com/golang/lint/golint
+# Fix that
+# go get golang.org/x/tools/cmd/godoc
+# go get github.com/golang/lint/golint
 
