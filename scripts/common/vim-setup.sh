b3a23424967e010c25ac47d6a52a0477fbc7c098
youshy <devilyoushy@gmail.com>
2019-08-01 11:29:26 +0100

    Muted vim setup
    I need to learn how to set it up myself first...

--- scripts/common/vim-setup.sh
+++ scripts/common/vim-setup.sh
@@ -3,13 +3,13 @@ echo "Setting up Vim"
 
 # Ultimate Vim
 
-git clone https://github.com/amix/vimrc.git ~/.vim_runtime
+# git clone https://github.com/amix/vimrc.git ~/.vim_runtime
 
-sh ~/.vim_runtime/install_awesome_vimrc.sh
+# sh ~/.vim_runtime/install_awesome_vimrc.sh
 
 # Maximum Awesome
 
-git clone https://github.com/square/maximum-awesome.git
+# git clone https://github.com/square/maximum-awesome.git
 
-cd maximum-awesome
-rake
+# cd maximum-awesome
+# rake
