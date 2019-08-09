e7fa72575160ccb0478aeff8ca79c9994a276bd1
Artur Kondas <devilyoushy@gmail.com>
2019-08-09 22:09:09 +0100

    Update setup for vim

--- scripts/common/vim-setup.sh
+++ scripts/common/vim-setup.sh
@@ -1,18 +1,10 @@
 echo
 echo "Setting up Vim"
 
-# Ultimate Vim
-
-# git clone https://github.com/amix/vimrc.git ~/.vim_runtime
-
-# sh ~/.vim_runtime/install_awesome_vimrc.sh
-
-# Maximum Awesome
-
-# git clone https://github.com/square/maximum-awesome.git
-
-# cd maximum-awesome
-# rake
-
 brew install macvim
 brew install luarocks
+
+# Install plug
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
